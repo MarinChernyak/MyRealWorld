c561e74ea4f5308c1439824597e8273911e2c564
ALEKSEY ULITSKY <74875761+MarinChernyak@users.noreply.github.com>
2025-05-26 09:44:13 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyRealWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
